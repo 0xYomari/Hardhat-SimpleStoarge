575026a5f2afb440f10ee541659604824ea168ff
Bijay Ghullu <bijayghullu@Bijays-MacBook-Pro.local>
2022-08-24 23:41:52 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x232235"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "asdg23232"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "32asdgasg"
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            //accounts: Thanks hardhat
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.9",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
